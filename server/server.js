71d9b38ea9ad74814449f58a0e982cedc6697728
Trang Lam <ms.tranglam@gmail.com>
2022-02-24 11:16:14 -0800

    add rollbar

--- server/server.js
+++ server/server.js
@@ -5,6 +5,17 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: 'a0ddf4b9e3604ccf8ea86d4505f5d869',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 const port = process.env.PORT || 4545
 
 app.listen(port, ()=>{ console.log(`take us to warp ${port}`)})
