5c027521cbbbf59d59df359238d079a1dc90a15c
Trang Lam <ms.tranglam@gmail.com>
2022-02-24 11:56:31 -0800

    edit

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+
+app.use(express.json())
+
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
+
+app.use(express.static(path.join(__dirname, '../public')))
 
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
@@ -15,6 +20,25 @@ const rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
+})
 
 const port = process.env.PORT || 4545
 
